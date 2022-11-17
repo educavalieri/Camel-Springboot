139823bbc81b2f05c714aac1e9e9a58af6de3787
educavalieri <eduardocavalieri@hotmail.com>
2022-11-17 15:09:46 -0300

    start project

---
+++ camel-a/src/test/java/com/camelExemaple/camela/CamelAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camelExemaple.camela;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
