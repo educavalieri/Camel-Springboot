98c8f3ab76aabf970a9dcbec499f0f71e24c74bc
educavalieri <eduardocavalieri@hotmail.com>
2022-12-01 19:54:44 -0300

    implement usable rest endpoint

--- rest/src/main/java/com/edu/rest/routers/GetRestRouter.java
+++ rest/src/main/java/com/edu/rest/routers/GetRestRouter.java
@@ -2,6 +2,8 @@ package com.edu.rest.routers;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.camel.model.rest.RestParamType;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -9,19 +11,29 @@ public class GetRestRouter extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
+//
+//        restConfiguration()
+//                .component("servlet")
+//                .port(8080)
+//                .host("localhost")
+//                .bindingMode(RestBindingMode.auto);
 
-        restConfiguration()
-                .host("localhost")
-                .port(8081)
-                .bindingMode(RestBindingMode.auto);
-
-        rest("/home")
+        rest()
                 .get("/user")
-                .routeId("get-rest-router")
+//                .routeId("get-rest-router")
+                .consumes(MediaType.APPLICATION_JSON_VALUE)
+//                .produces(MediaType.APPLICATION_JSON_VALUE)
+                .produces("text/plain")
+                .outType(String.class)
+                .param()
+                .name("name")
+                .type(RestParamType.query)
+                .endParam()
                 .to("direct:end-rest");
 
         from("direct:end-rest")
                 .routeId("end-rest")
+                .setBody(simple("oi meu caro ${header.name}"))
                 .log("${body}");
 
     }
